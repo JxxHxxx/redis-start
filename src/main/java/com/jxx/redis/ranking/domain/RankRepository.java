c2a027c235c7ab3aef63432ab1bb8da633428971
JxxHxxx <leesin5498@naver.com>
2023-06-25 05:54:02 +0900

    Feat/키가 존재할 떄 만 스코어 부여할 수 있도록 변경

--- src/main/java/com/jxx/redis/ranking/domain/RankRepository.java
+++ src/main/java/com/jxx/redis/ranking/domain/RankRepository.java
@@ -1,6 +1,8 @@
 package com.jxx.redis.ranking.domain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Repository;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class RankRepository {
@@ -17,7 +20,7 @@ public class RankRepository {
 
     public void save(Rank rank) {
         ZSetOperations<String, Rank> zSetOperations = redisTemplate.opsForZSet();
-        zSetOperations.add("storeRank", rank, 0);
+        zSetOperations.addIfAbsent(STORE_RANK_BOARD_KEY, rank, 0);
     }
 
     public Set<ZSetOperations.TypedTuple<Rank>> range(long start, long end) {
@@ -25,8 +28,19 @@ public class RankRepository {
         return zSetOperations.reverseRangeWithScores(STORE_RANK_BOARD_KEY, start, end);
     }
 
+    /**
+     * addIfAbsent -> return 해당 키가 존재하지 않을 경우(아래 메서드에선 rank) 키를 sorted-set에 추가하고 true를 반환한다.
+     * 만약 이미 키가 존재하다변 false를 반호나한다.
+     */
+
     public void score(Rank rank) {
         ZSetOperations<String, Rank> zSetOperations = redisTemplate.opsForZSet();
+        Boolean notExistedRankKey = zSetOperations.addIfAbsent(STORE_RANK_BOARD_KEY, rank, 0);
+
+        if (notExistedRankKey) {
+            throw new IllegalArgumentException("존재하지 않는 Rank 키 입니다.");
+        }
+
         zSetOperations.incrementScore(STORE_RANK_BOARD_KEY, rank, 1);
     }
 
